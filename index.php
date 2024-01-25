08e49d697cef07142ffa03868851f5a3c7f227d5
ZachBrannigann <300074789@fvtc.edu>
2024-01-24 20:32:22 -0600

    Homepage

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Zach's Homepage</title>
+        <link rel="stylesheet" type="text/css" href="css/base.css">
+    </head>
+
+    <body>
+    <header>
+        <h1>Welcome To The Struggle</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="/">Homepage</a> </li>
+            <li><a href="#">Loop Demo</a> </li>
+            <li><a href="#">Timer</a> </li>
+        </ul>
+    </nav>
+    <main>
+
+        <img src="images/PlaceHolder1.jpg" alt="my likeness" />
+
+        <p>I'm a software student with no background in computers just trying to make it.</p>
+    </main>
+    <footer>
+        &copy; <?= date("Y") ?> Zach Dodd
+    </footer>
+    </body>
+
+</html>
+
+
